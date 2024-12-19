9e692ce7a50f8f38fe81ef5a2e09bf3c215488fa
Bruna Liduario Moraes <bruna.liduario@ergbh.intra>
2024-12-19 11:04:00 -0300

    Service layer, component registration

---
+++ src/main/java/com/educandoweb/course/service/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entity.User;
+import com.educandoweb.course.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.get();
+	}
+
+}
