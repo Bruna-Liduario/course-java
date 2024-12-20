e9a76bea5685b0894b9c6e6251ec238cfef1d408
Bruna Liduario Moraes <bruna.liduario@ergbh.intra>
2024-12-20 10:15:54 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/service/UserService.java
+++ src/main/java/com/educandoweb/course/service/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entity.User;
 import com.educandoweb.course.repository.UserRepository;
+import com.educandoweb.course.service.exceptions.DatabaseException;
 import com.educandoweb.course.service.exceptions.ResourceNotFoundException;
 
 @Service
@@ -42,7 +45,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		try{
+			userRepository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		} 
 	}
 	
 
